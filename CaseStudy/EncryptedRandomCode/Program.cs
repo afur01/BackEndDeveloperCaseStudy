72ba932565575060eccdb8f6a1e393f13ea0920c
Ahmet Furkan Bozkurt <burislifurik@hotmail.com>
2024-01-24 22:47:27 +0300

    Back-End Developer Case Study Çalışması

---
+++ CaseStudy/EncryptedRandomCode/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptedRandomCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SecureCodeGenerator secureCode = new SecureCodeGenerator();
+            int numberOfcodes;
+
+            while (true) // Sonsuz döngü 
+            {
+                Console.WriteLine("...");
+                Console.Write("Lütfen Oluşturmak İstediğiniz Benzersiz kod adedini giriniz (0 çıkmak için) : ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out numberOfcodes)) 
+                {
+                    Console.WriteLine("Hatalı giriş! Lütfen bir sayı giriniz.");
+                    continue; 
+                }
+
+                if (numberOfcodes == 0)
+                {
+                    Console.WriteLine("Program sonlandırılıyor...");
+                    return; 
+                }
+
+                int counter = 0;
+
+                while (counter < numberOfcodes)
+                {
+                    var code = secureCode.GenerateCode();
+                    if (secureCode.CheckCode(code))
+                    {
+                        Console.WriteLine($"Code:{counter + 1} , {code}");
+                        counter++;
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class SecureCodeGenerator
+    {
+        private static readonly string charSet = "ACDEFGHKLMNPRTXYZ234579";
+
+        private static readonly Random random = new Random();
+
+        private HashSet<string> generatedCodes = new HashSet<string>();
+
+        // Güvenli rastgele kod üretir
+        public string GenerateCode()
+        {
+            byte[] bytes = new byte[4]; // 4 baytlık bir rastgele sayı için  
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes); // Güvenli bir şekilde rastgele baytlar üretilmesini sağlar
+            }
+
+            var randomNumber = BitConverter.ToUInt32(bytes, 0);
+            var code = new StringBuilder();
+
+            //rastgele üretilen bir sayıyı kullanarak charSet içerisinden karakter seçmek ve bu karakterlerle bir kod oluşturmak için kullandım
+            for (int i = 0; i < 8; i++)
+            {
+                code.Append(charSet[(int)(randomNumber % (uint)charSet.Length)]);
+                randomNumber /= (uint)charSet.Length;
+            }
+
+            string finalCode = code.ToString();
+            if (generatedCodes.Contains(finalCode))  
+            {
+                return GenerateCode();  // Bu Recursive metot ile benzersiz bir kod üretene kadar kendini tekrar edecek
+            }
+
+            generatedCodes.Add(finalCode);
+            return finalCode;
+        }
+
+        // Kodun geçerliliğini kontrol eder
+        public bool CheckCode(string code)
+        {
+            //kodun her bir karakterin ASCII değerlerinin toplamının asal sayı olup olmadığını kontrol ediyoruz
+            int sum = 0;
+            foreach (char c in code)
+            {
+                sum += c;
+            }
+            return IsPrime(sum);
+        }
+
+        //Verilen sayının asal olup olmadığını kontrol ediyoruz
+        private bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+
+
+}
---
+++ CaseStudy/ParsingJsonFile/Program.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ParsingJsonFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string jsonFilePath = @"D:\WEB\Rasgele Kod\ParsingJsonFile\response.json";  // jspn dosya yolu
+            
+            string outputFilePath = @"D:\WEB\Rasgele Kod\ParsingJsonFile\ticket.txt";  // çıktı dosya yolu
+
+            JsonParser.ParseAndWriteToJsonFile(jsonFilePath, outputFilePath);
+
+            Console.ReadLine();
+        }
+
+        public class JsonParser
+        {
+            public static void ParseAndWriteToJsonFile(string jsonFilePath, string outputFilePath)
+            {
+                // JSON dosyasını okur ve bir string olarak döndürüyor
+                string jsonContent = File.ReadAllText(jsonFilePath);
+
+                // JSON içeriğini parse edip JSON Array nesnesine dönüştürdük
+                JArray items = JArray.Parse(jsonContent);
+
+                // Y eksenindeki benzerlik için tolerans sınırını belirliyoruz
+                int yTolerance = 10;
+
+                // Her bir nesneyi y eksenindeki konumuna göre sıralanmış metinleri saklamak için bu koleksiyonu kullandım
+                var sortedItems = new SortedList<int, List<string>>();
+
+                //parse edilmiş JSON öğeleri üzerinde üzerinde işlem yapmak için döngüye aldık
+                foreach (JObject item in items)
+                {
+                    // "locale": "tr" özelliğini içeren öğeleri atladık
+                    if (item.ContainsKey("locale") && (string)item["locale"] == "tr")
+                    {
+                        continue;
+                    }
+
+                    //Her öğenin y eksenindeki konumunu y değerini alıyoruz
+                    int y = (int)item["boundingPoly"]["vertices"][0]["y"];
+
+                    // En yakın bulmak için sortedItems sözlüğündeki mevcut anahtarları kontrol ediyoruz.
+                    int closestY = -1;
+                    foreach (int key in sortedItems.Keys)
+                    {
+                        if (Math.Abs(y - key) <= yTolerance)
+                        {
+                            closestY = key;
+                            break;
+                        }
+                    }
+
+                    // Eğer yakın bir y konumu varsa mevcut satıra ekliyoruz , yoksa yeni satır oluşturuyoruz
+                    if (closestY != -1)
+                    {
+                        sortedItems[closestY].Add((string)item["description"]);
+                    }
+                    else
+                    {
+                        sortedItems.Add(y, new List<string> { (string)item["description"] });
+                    }
+                }
+
+                // Sonuçları numaralandırarak bir dosyaya yazıyoruz
+                List<string> outputLines = new List<string>();
+                int lineNumber = 1;
+                foreach (var line in sortedItems.Values)
+                {
+                    outputLines.Add($"{lineNumber}: {string.Join(" ", line)}");
+                    lineNumber++;
+                }
+
+                // Metin Yazma işlemi
+                File.WriteAllLines(outputFilePath, outputLines);
+            }
+        }
+
+
+
+    }
+}
